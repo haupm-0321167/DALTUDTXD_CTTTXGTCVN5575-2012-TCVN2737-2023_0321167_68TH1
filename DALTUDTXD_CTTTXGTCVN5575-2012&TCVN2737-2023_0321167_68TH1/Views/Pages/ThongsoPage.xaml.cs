8fd9cef09f15fa95771d67144c9a3b3e1b1d80de
haudz24052003-afk <haudz24052003@gmail.com>
2026-06-18 20:27:26 +0700

    HOÀN THÀNH 50% PAGE CHỌN XÀ GỒ

---
+++ DALTUDTXD_CTTTXGTCVN5575-2012&TCVN2737-2023_0321167_68TH1/Views/Pages/ThongsoPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DALTUDTXD_CTTTXGTCVN5575_2012_TCVN2737_2023_0321167_68TH1.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for ThongsoPage.xaml
+    /// </summary>
+    public partial class ThongsoPage : Page
+    {
+        public ThongsoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
