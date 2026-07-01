463a3ee6dae1bb97086c56c2f22fbaaba6e3a8d7
AnhNT0298568 <anhthi2082005@gmail.com>
2026-07-02 04:41:31 +0700

    70% NỘI DUNG XAGOMODEL

---
+++ DALTUDTXD_CTTTXGTCVN5575-2012&TCVN2737-2023_0321167_68TH1/Models/XagoModels.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALTUDTXD_CTTTXGTCVN5575_2012_TCVN2737_2023_0321167_68TH1.Models
+{
+    public class XagoModels : INotifyPropertyChanged
+    {
+
+        private int id;
+
+        private double extraWidth;
+
+        private double height, width, thickness, length, lip;
+
+        public double A { get; set; }
+        public double G { get; set; }
+
+        public double Ix { get; set; }
+        public double Iy { get; set; }
+
+        public double Wx { get; set; }
+        public double Wy { get; set; }
+
+
+        private string loai;
+        public string Loai
+        {
+            get => loai;
+            set { loai = value; OnChanged(nameof(Loai)); }
+        }
+        public double Radius { get; set; } = 5;
+        public int Id
+        {
+            get => id;
+            set { id = value; OnChanged(nameof(Id)); }
+        }
+
+        public double Height
+        {
+            get => height;
+            set { height = value; OnChanged(nameof(Height)); }
+        }
+        public double Width
+        {
+            get => width;
+            set { width = value; OnChanged(nameof(Width)); }
+        }
+        public double ExtraWidth
+        {
+            get => extraWidth;
+            set { extraWidth = value; OnChanged(nameof(ExtraWidth)); }
+        }
+        public double Thickness
+        {
+            get => thickness;
+            set { thickness = value; OnChanged(nameof(Thickness)); }
+        }
+        public double Length
+        {
+            get => length;
+            set { length = value; OnChanged(nameof(Length)); }
+        }
+        public double Lip
+        {
+            get => lip;
+            set { lip = value; OnChanged(nameof(Lip)); }
+        } // mép gập
+        public bool IsValid
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(Loai)) return false;
+
+                if (Height <= 0 || Width <= 0 || Thickness <= 0 || Length < 0)
+                    return false;
+
+                if (Loai.Contains("C") || Loai.Contains("Z"))
+                    return ExtraWidth >= 0;
+
+                return true;
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+
+    }
