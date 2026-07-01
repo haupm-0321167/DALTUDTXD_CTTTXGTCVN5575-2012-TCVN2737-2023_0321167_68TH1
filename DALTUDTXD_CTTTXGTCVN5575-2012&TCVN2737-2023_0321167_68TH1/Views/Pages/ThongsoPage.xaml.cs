5f685eaeff4b79df85c863eabfaa4f59ba0f42d4
haudz24052003-afk <haudz24052003@gmail.com>
2026-07-02 03:16:58 +0700

    HOÀN THÀNH 50% THONGSOPAGE.XAML.CS

--- DALTUDTXD_CTTTXGTCVN5575-2012&TCVN2737-2023_0321167_68TH1/Views/Pages/ThongsoPage.xaml.cs
+++ DALTUDTXD_CTTTXGTCVN5575-2012&TCVN2737-2023_0321167_68TH1/Views/Pages/ThongsoPage.xaml.cs
@@ -59,8 +59,177 @@ namespace DALTUDTXD_CTTTXGTCVN5575_2012_TCVN2737_2023_0321167_68TH1.Views.Pages
                 MessageBox.Show("Không đọc được file JSON!\n" + ex.Message);
             }
         }
+        private void rbt_huybo_Click(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).Close();
+        }
 
 
+
+
+
+
+
+
+        private void btn_them_click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                var selectedLoai = (cbb_LoaiXG.SelectedItem as ComboBoxItem)?.Content.ToString();
+                if (string.IsNullOrEmpty(selectedLoai))
+                {
+                    MessageBox.Show("Chọn loại xà gồ!");
+                    return;
+                }
+
+                XagoModels xg = new XagoModels()
+                {
+                    Id = id++,
+                    Height = double.Parse(txt_H.Text),
+                    Width = double.Parse(txt_B.Text),
+                    Lip = double.Parse(txt_C.Text),
+                    Thickness = double.Parse(txt_t.Text),
+                    Length = double.Parse(txt_D.Text),
+                    ExtraWidth = double.Parse(txt_B1.Text),
+
+                    A = xaGoDangChon.S,
+                    G = xaGoDangChon.P,
+                    Ix = xaGoDangChon.Jx,
+                    Iy = xaGoDangChon.Jy,
+                    Wx = xaGoDangChon.Wx,
+                    Wy = xaGoDangChon.Wy,
+
+                    Loai = selectedLoai
+                };
+
+                danhSach.Add(xg);
+                dgCot.SelectedItem = xg;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btn_xoa_click(object sender, RoutedEventArgs e)
+        {
+            if (dgCot.SelectedItem != null)
+            {
+                danhSach.Remove((XagoModels)dgCot.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Chọn dòng cần xóa!");
+            }
+        }
+
+        private void cbb_LoaiXG_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = cbb_LoaiXG.SelectedItem as ComboBoxItem;
+            if (item == null) return;
+
+            string selected = item.Content.ToString();
+
+            string loai = ((ComboBoxItem)cbb_LoaiXG.SelectedItem).Content.ToString();
+
+            switch (loai)
+            {
+                case "XG C dập nguội":
+                    LoadXaGo("xagocnguoi.json");
+                    break;
+
+                case "XG Z dập nguội":
+                    LoadXaGo("xagozdn.json");
+                    break;
+
+                case "XG C cán nóng":
+                    LoadXaGo("xagocnong.json");
+                    break;
+
+                case "XG Z cán nóng":
+                    LoadXaGo("xagozn.json");
+                    break;
+
+                case "XG thép hộp chữ nhật":
+                    LoadXaGo("xagohop.json");
+                    break;
+
+                case "XG thép hộp vuông":
+                    LoadXaGo("xagov.json");
+                    break;
+            }
+
+            DrawPreview2D();
+        }
+
+
+        private void LoadXaGo(string fileName)
+        {
+            try
+            {
+                string baseDir =
+                    AppDomain.CurrentDomain.BaseDirectory;
+
+                string projectPath =
+                    Directory.GetParent(baseDir)
+                    .Parent
+                    .Parent
+                    .FullName;
+
+                string path =
+                    System.IO.Path.Combine(
+                        projectPath,
+                        "Data",
+                        fileName);
+
+                string json =
+                    File.ReadAllText(path);
+
+                dsXaGo =
+                    JsonConvert.DeserializeObject<List<XaGo>>(json);
+
+                cbb_SoHieu.Items.Clear();
+
+                foreach (var xg in dsXaGo)
+                {
+                    cbb_SoHieu.Items.Add(xg.SoHieu);
+                }
+
+                cbb_SoHieu.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void cbb_SoHieu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbb_SoHieu.SelectedItem == null)
+                return;
+
+            string soHieu = cbb_SoHieu.SelectedItem.ToString();
+
+            xaGoDangChon = dsXaGo.FirstOrDefault(x => x.SoHieu == soHieu);
+
+            if (xaGoDangChon == null)
+                return;
+
+            txt_H.Text = xaGoDangChon.H.ToString();
+            txt_B.Text = xaGoDangChon.B.ToString();
+            txt_C.Text = xaGoDangChon.C.ToString();
+            txt_t.Text = xaGoDangChon.t.ToString();
+            txt_B1.Text = xaGoDangChon.B1.ToString();
+            txt_D.Text = xaGoDangChon.D.ToString();
+
+            
+            txtProp_A.Text = xaGoDangChon.S.ToString("0.00") + " cm²";
+            txtProp_G.Text = xaGoDangChon.P.ToString("0.00") + " kg/m";
+            txtProp_Ix.Text = xaGoDangChon.Jx.ToString("0.00") + " cm⁴";
+            txtProp_Iy.Text = xaGoDangChon.Jy.ToString("0.00") + " cm⁴";
+            txtProp_Wx.Text = xaGoDangChon.Wx.ToString("0.00") + " cm³";
+            txtProp_Wy.Text = xaGoDangChon.Wy.ToString("0.00") + " cm³";
+        }
 
     }
 }
