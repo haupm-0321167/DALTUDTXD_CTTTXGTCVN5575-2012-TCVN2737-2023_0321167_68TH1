2c84fdaaed86153747c83c62660495385259e045
haudz24052003-afk <haudz24052003@gmail.com>
2026-07-02 03:46:24 +0700

    HOÀN THÀNH PHẦN CS CUẢ INPUTBH.XAML.CS

--- DALTUDTXD_CTTTXGTCVN5575-2012&TCVN2737-2023_0321167_68TH1/Views/POPUP_PHAMHAU/Inputbh.xaml.cs
+++ DALTUDTXD_CTTTXGTCVN5575-2012&TCVN2737-2023_0321167_68TH1/Views/POPUP_PHAMHAU/Inputbh.xaml.cs
@@ -19,9 +19,27 @@ namespace DALTUDTXD_CTTTXGTCVN5575_2012_TCVN2737_2023_0321167_68TH1.Views.POPUP_
     /// </summary>
     public partial class Inputbh : Window
     {
+        public double B { get; private set; }
+        public double H { get; private set; }
         public Inputbh()
         {
             InitializeComponent();
+        }
+        private void BtnOK_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(txt_b.Text, out double b) &&
+                double.TryParse(txt_h.Text, out double h))
+            {
+                B = b;
+                H = h;
+
+                DialogResult = true;
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Nhập đúng giá trị b và h!");
+            }
         }
     }
 }
