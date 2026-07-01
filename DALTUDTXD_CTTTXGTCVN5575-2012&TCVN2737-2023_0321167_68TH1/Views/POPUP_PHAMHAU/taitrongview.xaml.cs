4495d8057ddcdd55a429edd2dc5d99f51440259f
haudz24052003-afk <haudz24052003@gmail.com>
2026-07-02 03:30:22 +0700

    HOÀN THÀNH GIAO DIỆN LIÊN HỆ CONTACTVIEW.XAML

---
+++ DALTUDTXD_CTTTXGTCVN5575-2012&TCVN2737-2023_0321167_68TH1/Views/POPUP_PHAMHAU/taitrongview.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DALTUDTXD_CTTTXGTCVN5575_2012_TCVN2737_2023_0321167_68TH1.Views.POPUP_PHAMHAU
+{
+    /// <summary>
+    /// Interaction logic for taitrongview.xaml
+    /// </summary>
+    public partial class taitrongview : Window
+    {
+        public taitrongview()
+        {
+            InitializeComponent();
+        }
+    }
+}
