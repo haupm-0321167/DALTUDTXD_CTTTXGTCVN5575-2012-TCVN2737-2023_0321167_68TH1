6ab9742a09940774a59183458a3bc6efb2900879
haudz24052003-afk <haudz24052003@gmail.com>
2026-07-02 04:05:05 +0700

    HOÀN THÀNH 40% PHẦN CS CỦA TAITRONGVIEW.XAML.CS

--- DALTUDTXD_CTTTXGTCVN5575-2012&TCVN2737-2023_0321167_68TH1/Views/POPUP_PHAMHAU/taitrongview.xaml.cs
+++ DALTUDTXD_CTTTXGTCVN5575-2012&TCVN2737-2023_0321167_68TH1/Views/POPUP_PHAMHAU/taitrongview.xaml.cs
@@ -156,5 +156,170 @@ namespace DALTUDTXD_CTTTXGTCVN5575_2012_TCVN2737_2023_0321167_68TH1.Views.POPUP_
             txt_diahinh.Text =
                 data.dia_hinh;
         }
+        private void TinhZe_Auto(object sender, EventArgs e)
+        {
+            if (txt_ze == null || txt_kze == null || txt_diahinh == null)
+            {
+                return;
+            }
+            if (B_Nha == 0 || H_Nha == 0)
+            {
+                Inputbh frm = new Inputbh();
+
+                if (frm.ShowDialog() == true)
+                {
+                    B_Nha = frm.B;
+                    H_Nha = frm.H;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            try
+            {
+                double Z = TinhZe(B_Nha, H_Nha);
+
+                txt_ze.Text = Z.ToString("0.00");
+
+                double kZe = Tinh_kZe(Z, txt_diahinh.Text);
+
+                txt_kze.Text = kZe.ToString("0.000");
+            }
+            catch
+            {
+
+            }
+        }
+        private double TinhZe(double b, double h)
+        {
+
+            if (h <= b)
+                return h;
+
+
+            if (h <= 2 * b)
+                return b;
+
+            return h;
+        }
+        private double Tinh_kZe(double Ze, string diaHinh)
+        {
+            double zg = 0;
+            double zmin = 0;
+            double alpha = 0;
+
+            switch (diaHinh)
+            {
+                case "A":
+                    zg = 213.36;
+                    zmin = 2.13;
+                    alpha = 11.5;
+                    break;
+
+                case "B":
+                    zg = 274.32;
+                    zmin = 4.57;
+                    alpha = 9.5;
+                    break;
+
+                case "C":
+                    zg = 365.76;
+                    zmin = 9.14;
+                    alpha = 7.0;
+                    break;
+
+                default:
+                    return 1.0;
+            }
+
+
+            if (Ze < zmin)
+                Ze = zmin;
+
+            double kZe = 2.01 *
+                         Math.Pow(
+                             Ze / zg,
+                             2.0 / alpha);
+
+            return kZe;
+        }
+
+
+
+        private void Tinhc_Auto(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbb_loainha == null || txt_c == null)
+                return;
+            if (cbb_loainha.SelectedItem == null)
+                return;
+            string loaiNha =
+                ((ComboBoxItem)cbb_loainha.SelectedItem)
+                .Content.ToString();
+            double c = 0;
+
+            switch (loaiNha)
+            {
+                case "Nhà mái bằng":
+                    if (cbb_vunggio.SelectedItem != null && ((ComboBoxItem)cbb_vunggio.SelectedItem).Content.ToString() == "Vùng F")
+                        c = -1.2;
+                    if (cbb_vunggio.SelectedItem != null && ((ComboBoxItem)cbb_vunggio.SelectedItem).Content.ToString() == "Vùng G")
+                        c = -0.9;
+                    if (cbb_vunggio.SelectedItem != null && ((ComboBoxItem)cbb_vunggio.SelectedItem).Content.ToString() == "Vùng H")
+                        c = -0.7;
+                    if (cbb_vunggio.SelectedItem != null && ((ComboBoxItem)cbb_vunggio.SelectedItem).Content.ToString() == "Vùng I")
+                        c = -0.2;
+
+
+                    break;
+
+                case "Nhà 1 mái dốc":
+
+                    if (cbb_vunggio.SelectedItem != null && ((ComboBoxItem)cbb_vunggio.SelectedItem).Content.ToString() == "Vùng F")
+                        c = -2.31;
+                    if (cbb_vunggio.SelectedItem != null && ((ComboBoxItem)cbb_vunggio.SelectedItem).Content.ToString() == "Vùng G")
+                        c = -1.3;
+                    if (cbb_vunggio.SelectedItem != null && ((ComboBoxItem)cbb_vunggio.SelectedItem).Content.ToString() == "Vùng H")
+                        c = -0.81;
+
+
+                    break;
+
+                case "Nhà 2 mái dốc":
+
+                    if (cbb_vunggio.SelectedItem != null && ((ComboBoxItem)cbb_vunggio.SelectedItem).Content.ToString() == "Vùng F")
+                        c = -1.67;
+                    if (cbb_vunggio.SelectedItem != null && ((ComboBoxItem)cbb_vunggio.SelectedItem).Content.ToString() == "Vùng G")
+                        c = -1.3;
+                    if (cbb_vunggio.SelectedItem != null && ((ComboBoxItem)cbb_vunggio.SelectedItem).Content.ToString() == "Vùng H")
+                        c = -0.69;
+                    if (cbb_vunggio.SelectedItem != null && ((ComboBoxItem)cbb_vunggio.SelectedItem).Content.ToString() == "Vùng I")
+                        c = -0.59;
+
+                    break;
+
+                case "Nhà mái vòm":
+
+                    if (cbb_vunggio.SelectedItem != null && ((ComboBoxItem)cbb_vunggio.SelectedItem).Content.ToString() == "Vùng A")
+                        c = -1.2;
+                    if (cbb_vunggio.SelectedItem != null && ((ComboBoxItem)cbb_vunggio.SelectedItem).Content.ToString() == "Vùng B")
+                        c = -0.87;
+                    if (cbb_vunggio.SelectedItem != null && ((ComboBoxItem)cbb_vunggio.SelectedItem).Content.ToString() == "Vùng C")
+                        c = -0.4;
+                    if (cbb_vunggio.SelectedItem != null && ((ComboBoxItem)cbb_vunggio.SelectedItem).Content.ToString() == "-")
+                        c = -0.4;
+
+                    break;
+
+                case "Nhà mái che":
+                    c = -1.3;
+                    break;
+            }
+
+            txt_c.Text = c.ToString("0.##");
+
+            TinhZe_Auto(null, null);
+
+        }
     }
 }
