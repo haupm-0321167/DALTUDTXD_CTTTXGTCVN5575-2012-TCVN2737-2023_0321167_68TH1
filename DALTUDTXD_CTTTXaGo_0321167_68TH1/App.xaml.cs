d6585432a1ee0bdf115d64c452fe3ab6d5f63e67
AnhNK0298368 <nguyenkimanh2005123@gmail.com>
2026-07-03 00:32:01 +0700

    Add project files.

---
+++ DALTUDTXD_CTTTXaGo_0321167_68TH1/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DALTUDTXD_CTTTXaGo_0321167_68TH1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
