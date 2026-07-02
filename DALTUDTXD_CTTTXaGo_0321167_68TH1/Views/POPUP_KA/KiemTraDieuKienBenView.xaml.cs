a134ae9a1c282b1a30e2f66bff6104839ba6eedb
AnhNK0298368 <nguyenkimanh2005123@gmail.com>
2026-07-03 01:35:54 +0700

    HOÀN THÀNH 50% PHẦN KIEMTRADIEUKIENBENVIEW.XAML.CS CỦA VIEWS TRONG POPUP KA .

--- DALTUDTXD_CTTTXaGo_0321167_68TH1/Views/POPUP_KA/KiemTraDieuKienBenView.xaml.cs
+++ DALTUDTXD_CTTTXaGo_0321167_68TH1/Views/POPUP_KA/KiemTraDieuKienBenView.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DALTUDTXD_CTTTXaGo_0321167_68TH1.Data;
+using DALTUDTXD_CTTTXaGo_0321167_68TH1.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +22,65 @@ namespace DALTUDTXD_CTTTXaGo_0321167_68TH1.Views.POPUP_KA
     /// </summary>
     public partial class KiemTraDieuKienBenView : Window
     {
+        private ObservableCollection<KiemTraBen> dsKiemTra =
+           new ObservableCollection<KiemTraBen>();
         public KiemTraDieuKienBenView()
         {
             InitializeComponent();
+
+            dgKiemTra.ItemsSource = dsKiemTra;
+            HienThiToHop();
+        }
+
+        private void HienThiToHop()
+        {
+            dsKiemTra.Clear();
+
+
+            foreach (var th in GlobalData.DsNoiLucTinhToan)
+            {
+                double sigmaTd;
+
+                if (GlobalData.B1 == 0)
+                {
+                    sigmaTd = 100 * (th.Mx / GlobalData.Wx + th.My / GlobalData.Wy);
+                }
+                else
+                {
+                    sigmaTd = 100 * (0.5 * th.Mx / GlobalData.Wx + 0.5 * th.My / GlobalData.Wy);
+                }
+
+                dsKiemTra.Add(new KiemTraBen()
+                {
+                    ToHop = th.Truonghop,
+                    SigmaTd = Math.Round(sigmaTd, 2),
+                    DauSoSanh = "<=",
+                    SigmaChoPhep = 0,
+                    NhanXet = ""
+                });
+            }
+        }
+
+
+        private void cbb_LoaiThep_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            if (cbb_LoaiThep.SelectedItem == null)
+                return;
+
+            string tenThep =
+                ((ComboBoxItem)cbb_LoaiThep.SelectedItem)
+                .Content.ToString();
+
+            double fy = LayCuongDoThep(tenThep);
+            GlobalData.Fy = fy;
+            HienThiKetQua(fy);
+
+
+        }
+        private void btn_KiemTraVong_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
