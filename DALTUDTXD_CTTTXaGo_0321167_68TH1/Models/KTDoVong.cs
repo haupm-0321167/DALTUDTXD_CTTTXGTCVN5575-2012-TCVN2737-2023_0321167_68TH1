765c436ad704801e17fcb336ae080415129784bb
AnhNK0298368 <nguyenkimanh2005123@gmail.com>
2026-07-03 02:21:35 +0700

    HOÀN THIỆN PHẦN KTDOVONG.CS CỦA MODELS .

---
+++ DALTUDTXD_CTTTXaGo_0321167_68TH1/Models/KTDoVong.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALTUDTXD_CTTTXaGo_0321167_68TH1.Models
+{
+    public class KTDoVong
+    {
+        public string ToHop { get; set; }
+
+        public string ViTri { get; set; }
+
+        public int SoDiemGiang { get; set; }
+
+        public double? Fx { get; set; }
+
+        public double? Fy { get; set; }
+
+        public double? Fv { get; set; }
+        public double DoVongXG { get; set; }
+
+        public string SoSanh { get; set; }
+
+        public double DoVongChoPhep { get; set; }
+
+        public string NhanXet1 { get; set; }
+    }
+}
