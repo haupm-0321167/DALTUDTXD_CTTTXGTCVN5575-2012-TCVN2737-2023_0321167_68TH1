1e7767545179c0e48897e496a27fd697d6eb3c63
AnhNK0298368 <nguyenkimanh2005123@gmail.com>
2026-07-03 02:14:31 +0700

    HOÀN THIỆN PHẦN KIEMTRABEN.CS CỦA MODELS .

---
+++ DALTUDTXD_CTTTXaGo_0321167_68TH1/Models/KiemTraBen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALTUDTXD_CTTTXaGo_0321167_68TH1.Models
+{
+    internal class KiemTraBen
+    {
+        public string ToHop { get; set; }
+
+        public double SigmaTd { get; set; }
+
+        public string DauSoSanh { get; set; } = "<=";
+
+        public double SigmaChoPhep { get; set; }
+
+        public string NhanXet { get; set; }
+    }
+}
